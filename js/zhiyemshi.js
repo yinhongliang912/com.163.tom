5bee44f49d5877954bd40736f0b20a863dc1bd3e
yinhongliang912 <2993636628@qq.com>
2020-05-03 21:00:40 +0800

    提交作业

--- js/zhiyemshi.js
+++ js/zhiyemshi.js
@@ -1,3 +1,13 @@
-$(".ul li").click(function () {
-  $('.ul li').show();
+$(".ul-er").click(function () {
+  $(".ul-yi i").hide();
+  $(".ul-yi b").hide();
+  $(".youbian").hide();
+  $(".san-san").hide();
+  $(".san-si").hide();
+  $(".qh-yi").show();
+  $(".ul-er i").show();
+  $(".ul-er b").show();
+  $(".san-wu").show();
+  $(".youbiant").show();
+  $(".youbiant img").show();
 });
